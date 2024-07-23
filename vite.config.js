0b48f41459f54d27094cf2436abc443c6911d2fc
Zoro 5689 <ms9799630@gmail.com>
2024-07-23 20:03:34 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  build: {
+    rollupOptions: {
+      external: ['react-icons/io']
+    }
+  }
+}
+
+
+
